2cfc705c20420664f72455f6856ef789e242057c
jp-1998fw <129400679+jp-1998fw@users.noreply.github.com>
2023-04-09 12:55:15 +0200

    Adjusted for cluster use and only one input file

--- read_wrfout.py
+++ read_wrfout.py
@@ -7,17 +7,17 @@ Created on Fri Mar 10 09:24:26 2023
 """
 # %% Settings
 
-agpath = "W://"
-homepath = "Y://"
-datapath = "Z://"
+# agpath = "W://"
+# homepath = "Y://"
+# datapath = "Z://"
 
 # agpath = "/home/johannes/Data_Cluster/"
 # homepath ="/home/johannes/Cluster_Home/"
 # datapath = "/home/johannes/Data_Personal/" 
 
-# agpath = "/nfs/group/wesys_offshore/"
-# homepath = "/user/need3104/"
-# datapath = "/nfs/data/need3104/"
+agpath = "/nfs/group/wesys_offshore/"
+homepath = "/user/need3104/"
+datapath = "/nfs/data/need3104/"
 
 # %% Imports
 from netCDF4 import Dataset
@@ -33,16 +33,14 @@ import cartopy.feature as cfeature
 
 # %% Main
 
-ptf = r"C:\Users\johan\Downloads\MOP1VD~T.NC"
-
-ncfile = Dataset(r"Y:\wrfout1.nc")
-ncfile2 = Dataset(r"Y:\wrfout_d03_2018-02-16_18-00-00")
+# ncfile = Dataset(r"Y:\wrfout1.nc")
+ncfile2 = Dataset(r"wrfout_d03_2018-02-16_18-00-00")
 
 # Get the WRF variables
 
-z = getvar(ncfile, "z")
-wspd =  getvar(ncfile, "wspd_wdir", units="kt")[0,:]
-wspd_125m = interplevel(wspd, z, 125)
+# z = getvar(ncfile, "z")
+# wspd =  getvar(ncfile, "wspd_wdir", units="kt")[0,:]
+# wspd_125m = interplevel(wspd, z, 125)
 
 z2 = getvar(ncfile2, "z")
 wspd2 =  getvar(ncfile2, "wspd_wdir", units="kt")[0,:]
@@ -50,8 +48,8 @@ wspd_125m2 = interplevel(wspd2, z2, 125)
 
 
 
-plt.scatter(wspd_125m.XLONG, wspd_125m.XLAT, c=wspd_125m)
-plt.show()
+# plt.scatter(wspd_125m.XLONG, wspd_125m.XLAT, c=wspd_125m)
+# plt.show()
 
 plt.scatter(wspd_125m2.XLONG, wspd_125m2.XLAT, c=wspd_125m2)
 plt.show()
