bf2a0d711b3e41d729357471a335cdb7c47e83b5
Johannes Paulsen <johannes.paulsen@uni-oldenburg.de>
2023-04-11 18:21:16 +0200

    Back to old way of doing things + adjustable z

--- read_wrfout.py
+++ read_wrfout.py
@@ -31,10 +31,10 @@ from matplotlib.cm import get_cmap
 
 # %% Read wrf file
 
-ptf = "/gss/work/need3104/JOHANNES/WRF_XWAKES_180216/WRF_WT_on/"
+ptf = "/user/need3104/run1/"
 
 # ncfile = Dataset(r"Y:\wrfout1.nc")
-wrfin = Dataset(ptf + "/wrfout_d03_2018-02-16_18-00-00")
+wrfin = Dataset(ptf + "wrfout_d03_2018-02-16_18-00-00")
 
 # Get the WRF variables
 
@@ -48,12 +48,34 @@ wrfin = Dataset(ptf + "/wrfout_d03_2018-02-16_18-00-00")
 # Extract the wind speed data for a specific time and height level
 time_index = 0 # Set the time index to the first time step
 height_index = wrf.getvar(wrfin, "height_agl").argmin(dim='bottom_top') # Set the height index to the lowest level
-u, v = wrf.getvar(wrfin, "uvmet_wspd_wdir", timeidx=time_index)
+u, v,  = wrf.getvar(wrfin, "uvmet_wspd_wdir", timeidx=time_index)
 wind_speed = wrf.getvar(wrfin, "wspd_wdir", timeidx=time_index)[height_index,:]
 
 # Create a plot of the wind speed
-plt.plot(wind_speed)
+plt.plot(wrf.to_np(wind_speed)[:,:,5])
+
 plt.title('Wind Speed at Time Index {} and Height Level {}'.format(time_index, height_index))
 plt.xlabel('X Position')
 plt.ylabel('Y Position')
 plt.show()
+
+# %% Try different approach 
+
+height=120
+
+z = wrf.getvar(wrfin, "z")
+wspd =  wrf.getvar(wrfin, "wspd_wdir", units="m/s")[0,:]
+wspd_interp = wrf.interplevel(wspd, z, height)
+
+
+
+# plt.scatter(wspd_125m.XLONG, wspd_125m.XLAT, c=wspd_125m)
+# plt.show()
+
+plt.scatter(wspd_interp.XLONG, wspd_interp.XLAT, 
+            c=wspd_interp, vmin=0, vmax=15)
+plt.xlim([5,10])
+plt.ylim([54.5, 55.5])
+plt.colorbar()
+plt.show()
+
