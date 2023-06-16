085bc39b689a3978ed6284e1e37ee0aa189afd47
jp-1998fw <129400679+jp-1998fw@users.noreply.github.com>
2023-06-16 11:12:38 +0200

    Difference wind field added

--- read_wrfout.py
+++ read_wrfout.py
@@ -7,17 +7,17 @@ Created on Fri Mar 10 09:24:26 2023
 """
 # %% Settings
 
-# agpath = "W://"
-# homepath = "Y://"
-# datapath = "Z://"
+agpath = "W://"
+homepath = "Y://"
+datapath = "Z://"
 
 # agpath = "/home/johannes/Data_Cluster/"
 # homepath ="/home/johannes/Cluster_Home/"
 # datapath = "/home/johannes/Data_Personal/" 
 
-agpath = "/nfs/group/wesys_offshore/"
-homepath = "/user/need3104/"
-datapath = "/nfs/data/need3104/"
+# agpath = "/nfs/group/wesys_offshore/"
+# homepath = "/user/need3104/"
+# datapath = "/nfs/data/need3104/"
 
 # %% Imports
 from netCDF4 import Dataset
@@ -33,23 +33,46 @@ import cartopy.feature as cfeature
 
 # %% Main
 
-# ncfile = Dataset(r"Y:\wrfout1.nc")
-ncfile2 = Dataset(r"wrfout_d03_2018-02-16_18-00-00")
+os.chdir(homepath)
+
+# ncfile = Dataset(f"wrfout_d03_2018-12-28_00-00-00_WT")
+ncfile = Dataset(f"wrfout_d03_2018-12-28_2022_WT_smallerdt")
+ncfile2 = Dataset(f"wrfout_d03_2018-12-28_00-00-00")
 
 # Get the WRF variables
 
-# z = getvar(ncfile, "z")
-# wspd =  getvar(ncfile, "wspd_wdir", units="kt")[0,:]
-# wspd_125m = interplevel(wspd, z, 125)
+z = getvar(ncfile, "z")
+wspd_125m = interplevel(wspd, z, 125)
 
 z2 = getvar(ncfile2, "z")
-wspd2 =  getvar(ncfile2, "wspd_wdir", units="kt")[0,:]
-wspd_125m2 = interplevel(wspd2, z2, 125)
+wspd2 =  getvar(ncfile2, "wspd_wdir", units="m/s")[0,:]
+wspd_125m2 = interplevel(wspd2, z2, 120)
 
 
 
 # plt.scatter(wspd_125m.XLONG, wspd_125m.XLAT, c=wspd_125m)
 # plt.show()
 
-plt.scatter(wspd_125m2.XLONG, wspd_125m2.XLAT, c=wspd_125m2)
+plt.scatter(wspd_125m.XLONG, wspd_125m.XLAT, c=wspd_125m, )
+plt.xlabel("Longitude / °")
+plt.ylabel("Latitude / °")
+
+plt.colorbar()
 plt.show()
+
+plt.scatter(wspd_125m2.XLONG, wspd_125m2.XLAT, c=wspd_125m2, vmin=0, vmax=25)
+plt.xlabel("Longitude / °")
+plt.ylabel("Latitude / °")
+plt.colorbar()
+plt.show()
+
+
+plt.scatter(wspd_125m2.XLONG, wspd_125m2.XLAT, c=wspd_125m2 - wspd_125m, vmin=-25, vmax=25)
+plt.xlabel("Longitude / °")
+plt.ylabel("Latitude / °")
+plt.colorbar()
+plt.show()
+
+
+
+# %%
